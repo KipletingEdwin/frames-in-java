94d87814f026cdc1f0d5d778502e426418a814be
Kipleting Edwin <kipletingedwin4@gmail.com>
2024-07-30 10:18:13 +0300

    creates and intsnce of JFrame and makes it visible

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,14 @@
+import javax.swing.*;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+
+        //JFrame = a GUI window to add components to
+        JFrame frame = new JFrame(); //creates a frame
+        frame.setSize(500,500);
+        frame.setVisible(true); //make frame visible
 
     }
 }
